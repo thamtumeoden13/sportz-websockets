9d926d1d2cc25e04ad0d99d3a52518a06d42d745
BlackCat13 <detective.blackcat13@gmail.com>
2026-03-01 15:10:29 +0700

    inital

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the server!');
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
